ab67ddc790bde7c2f310a0fe37338feaeeb009df
Swiish21 <154547110+Swiish21@users.noreply.github.com>
2024-07-07 20:55:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let score = 80;
+
+if (score >= 80); {
+    console.log("You got an A!")
+}
+elseif (score < 80); {
+    console.log("You got an A-")
+}
+elseif (score < 75); {
+    console.log ("You got a B+")
+}
+elseif (score < 70); {
+    console.log ("You got a B(Plain)")
+}
+elseif (score < 65); {
+    console.log ("You got a B+")
+}
+elseif (score < 60); {
+    console.log ("You got a B-")
+}
+elseif (score < 55); {
+    console.log ("You got a C+")
+}
+elseif (score < 50); {
+    console.log ("You got a C(Plain)")
+}
+elseif (score < 45); {
+    console.log ("You got a C-")
+}
+elseif (score < 40); {
+    console.log ("You got a D+")
+}
+elseif (score < 35); {
+    console.log ("You got a D")
+}
+elseif (score < 30); {
+    console.log ("You got a D-")
+}
+elseif (score < 25); {
+    console.log ("You got an E")
+}
+elseif (score < 10); {
+    console.log ("You got an F")
+}
